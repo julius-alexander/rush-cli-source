497f2f75e1f79e8f229e901b42c6e25cd05901ea
johngarzon-ferrer <jgarzonferrer@gmail.com>
2024-10-17 16:51:40 -0400

    [Removed] Some comments

--- rush_processing.c
+++ rush_processing.c
@@ -4,11 +4,6 @@
  *
  * This file is part of rush, Rapid UNIX Shell
  *
- * Project 1 for COP 4600 at the University of South Florida, Tampa
- *
- * Author: John Garzon-Ferrer
- * NetID: garzonferrer@usf.edu
- *
  * Date: 13 October 2024
  *
  * License: This project is for educational purposes only and is not intended for redistribution.
